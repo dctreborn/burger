a3e88c2e2a880c1c5df484196e0af661326c5ed0
rukaroa <rukaroa@gmail.com>
2017-04-27 19:48:57 -0500

    orm done

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,29 @@
 var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
 var orm = {
     selectAll: function(table, cb) {
     	var query = "SELECT * FROM " + table;
@@ -18,7 +42,7 @@ var orm = {
         query += table;
         query += " (" + cols.toString() + ") ";
         query += "VALUES";
-        query += " (" + values.toString() + ") ";
+        query += " (" + printQuestionMarks(values.length) + ") ";
 
         console.log(query);
 
@@ -28,11 +52,19 @@ var orm = {
             cb(results);
         });
     },
-    updateOne: function() {
-        //UPDATE tablename SET 
-    	var query = ;
+    updateOne: function(table, objColVals, condition, cd) {
+        //'UPDATE users SET foo = ?, bar = ?, baz = ? WHERE id = ?'
+    	var query = "SET";
+        query += objToSql(objColVals);
+        query += " WHERE " + condition;
 
         console.log(query);
+
+        connection.query(query, function(err, results){
+            if (err) throw err;
+
+            cb(results);
+        });
     }
 };
 
