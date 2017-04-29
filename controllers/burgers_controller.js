fc3364e804e50b66792cee656f76599d26a59522
rukaroa <rukaroa@gmail.com>
2017-04-29 12:37:46 -0500

    controller set up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,29 @@ var router = app.Router();
 
 var burger = require("../models.burger.js");
 
-router.get()
+router.get("/", function(req, res) {
+	burger.selectAll(function(data){
+		vars hbsObj = {
+			burgers: data
+		};
+		res.render("index", hbsObj);
+	});
+});
+
+router.post("/", function(req, res){
+	burger.create(["burger_name", "devoured"],
+		[req.body.burger_name, req.body.devoured],
+		function(){
+			res.redirect("/");
+		});
+});
+
+router.put("/:id", function(req, res){
+	var condition = "id = " + req.params.id;
+
+	burger.update({
+		devoured: req.body.devoured
+	}, condition, function(){
+		res.redirect("/");
+	});
+});
